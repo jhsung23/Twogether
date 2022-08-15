dd469bb0da7d0e9bd4a35bc8d4a493f0db84ce4f
sungjihyun <jhsung23@gmail.com>
2022-08-15 16:06:29 +0900

    statusbar text color modified

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './screens/RootStack';
 import {UserContextProvider} from './contexts/UserContext';
 
 function App() {
+  StatusBar.setBarStyle('dark-content');
   return (
     <UserContextProvider>
       <NavigationContainer>
