c194278688b3760748e1f3d550bce315f9717ae3
Jiwoon2 <dkswjsgkek@naver.com>
2022-08-17 17:27:17 +0900

    KeyboardAvoid
    입력창 키보드 가림 수정

--- components/RegisterBaby.js
+++ components/RegisterBaby.js
@@ -6,6 +6,8 @@ import {
   TextInput,
   TouchableWithoutFeedback,
   Keyboard,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
 
 import {useUserContext} from '../contexts/UserContext';
@@ -56,108 +58,112 @@ function RegisterBaby({onSubmit, babyForm, onChange, additional}) {
           // eslint-disable-next-line react-native/no-inline-styles
           <View style={{margin: 5}} />
         )}
-        <View style={styles.wrapper}>
-          <>
-            <TextInput
-              style={styles.input}
-              placeholder="아기 이름"
-              placeholderTextColor="#919191"
-              value={baby.name}
-              onChangeText={value => {
-                updateBabyForm('name', value);
-                onChange ? onChange('name', value) : null;
-              }}
-            />
-            <TextInput
-              // eslint-disable-next-line react-native/no-inline-styles
-              style={[styles.input, {marginStart: 20}]}
-              placeholder="몇째인가요?"
-              placeholderTextColor="#919191"
-              keyboardType="numeric"
-              value={baby.order}
-              onChangeText={value => {
-                updateBabyForm('order', value);
-                onChange ? onChange('order', value) : null;
-              }}
-            />
-            <Text style={styles.unitText}>째</Text>
-          </>
-        </View>
+        <KeyboardAvoidingView
+          keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 350}
+          behavior={'padding'}>
+          <View style={styles.wrapper}>
+            <>
+              <TextInput
+                style={styles.input}
+                placeholder="아기 이름"
+                placeholderTextColor="#919191"
+                value={baby.name}
+                onChangeText={value => {
+                  updateBabyForm('name', value);
+                  onChange ? onChange('name', value) : null;
+                }}
+              />
+              <TextInput
+                // eslint-disable-next-line react-native/no-inline-styles
+                style={[styles.input, {marginStart: 20}]}
+                placeholder="몇째인가요?"
+                placeholderTextColor="#919191"
+                keyboardType="numeric"
+                value={baby.order}
+                onChangeText={value => {
+                  updateBabyForm('order', value);
+                  onChange ? onChange('order', value) : null;
+                }}
+              />
+              <Text style={styles.unitText}>째</Text>
+            </>
+          </View>
 
-        <View style={styles.wrapper}>
-          <>
-            <TextInput
-              style={styles.input}
-              placeholder="생년"
-              placeholderTextColor="#919191"
-              keyboardType="numeric"
-              value={baby.birthYear}
-              onChangeText={value => {
-                updateBabyForm('birthYear', value);
-                onChange ? onChange('birthYear', value) : null;
-              }}
-            />
-            <Text style={styles.unitText}>년</Text>
-          </>
-          <>
-            <TextInput
-              style={styles.input}
-              placeholder="생월"
-              placeholderTextColor="#919191"
-              keyboardType="numeric"
-              value={baby.birthMonth}
-              onChangeText={value => {
-                updateBabyForm('birthMonth', value);
-                onChange ? onChange('birthMonth', value) : null;
-              }}
-            />
-            <Text style={styles.unitText}>월</Text>
-          </>
-          <>
-            <TextInput
-              style={styles.input}
-              placeholder="생일"
-              placeholderTextColor="#919191"
-              keyboardType="numeric"
-              value={baby.birthDay}
-              onChangeText={value => {
-                updateBabyForm('birthDay', value);
-                onChange ? onChange('birthDay', value) : null;
-              }}
-            />
-            <Text style={styles.unitText}>일</Text>
-          </>
-        </View>
-        <View style={styles.wrapper}>
-          <>
-            <TextInput
-              style={styles.input}
-              placeholder="키"
-              placeholderTextColor="#919191"
-              keyboardType="numeric"
-              value={baby.height}
-              onChangeText={value => {
-                updateBabyForm('height', value);
-                onChange ? onChange('height', value) : null;
-              }}
-            />
-            <Text style={styles.unitText}>cm</Text>
-          </>
-          <>
-            <TextInput
-              style={styles.input}
-              placeholder="몸무게"
-              placeholderTextColor="#919191"
-              keyboardType="numeric"
-              value={baby.weight}
-              onChangeText={value => {
-                updateBabyForm('weight', value);
-                onChange ? onChange('weight', value) : null;
-              }}
-            />
-            <Text style={styles.unitText}>kg</Text>
-          </>
-        </View>
+          <View style={styles.wrapper}>
+            <>
+              <TextInput
+                style={styles.input}
+                placeholder="생년"
+                placeholderTextColor="#919191"
+                keyboardType="numeric"
+                value={baby.birthYear}
+                onChangeText={value => {
+                  updateBabyForm('birthYear', value);
+                  onChange ? onChange('birthYear', value) : null;
+                }}
+              />
+              <Text style={styles.unitText}>년</Text>
+            </>
+            <>
+              <TextInput
+                style={styles.input}
+                placeholder="생월"
+                placeholderTextColor="#919191"
+                keyboardType="numeric"
+                value={baby.birthMonth}
+                onChangeText={value => {
+                  updateBabyForm('birthMonth', value);
+                  onChange ? onChange('birthMonth', value) : null;
+                }}
+              />
+              <Text style={styles.unitText}>월</Text>
+            </>
+            <>
+              <TextInput
+                style={styles.input}
+                placeholder="생일"
+                placeholderTextColor="#919191"
+                keyboardType="numeric"
+                value={baby.birthDay}
+                onChangeText={value => {
+                  updateBabyForm('birthDay', value);
+                  onChange ? onChange('birthDay', value) : null;
+                }}
+              />
+              <Text style={styles.unitText}>일</Text>
+            </>
+          </View>
+          <View style={styles.wrapper}>
+            <>
+              <TextInput
+                style={styles.input}
+                placeholder="키"
+                placeholderTextColor="#919191"
+                keyboardType="numeric"
+                value={baby.height}
+                onChangeText={value => {
+                  updateBabyForm('height', value);
+                  onChange ? onChange('height', value) : null;
+                }}
+              />
+              <Text style={styles.unitText}>cm</Text>
+            </>
+            <>
+              <TextInput
+                style={styles.input}
+                placeholder="몸무게"
+                placeholderTextColor="#919191"
+                keyboardType="numeric"
+                value={baby.weight}
+                onChangeText={value => {
+                  updateBabyForm('weight', value);
+                  onChange ? onChange('weight', value) : null;
+                }}
+              />
+              <Text style={styles.unitText}>kg</Text>
+            </>
+          </View>
+        </KeyboardAvoidingView>
         {additional ? (
           <>
             {/*eslint-disable-next-line react-native/no-inline-styles*/}
